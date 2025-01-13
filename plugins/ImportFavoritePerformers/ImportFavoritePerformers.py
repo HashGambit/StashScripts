36a633e725ac22c9465c6578a723cabe3c690e9c
HashGambit77 <HashGambit77@gmail.com>
2025-01-13 17:34:09 -0500

    feat: enhance ImportFavoritePerformers plugin with error handling and improved data processing

--- plugins/ImportFavoritePerformers/ImportFavoritePerformers.py
+++ plugins/ImportFavoritePerformers/ImportFavoritePerformers.py
@@ -1,13 +1,22 @@
-import requests
-import json
-import sys
-import stashapi.log as log
+import sys, json, requests
 
-from stashapi.stashapp import StashInterface
+try:
+    import stashapi.log as log
+    from stashapi.stashapp import StashInterface
+except ModuleNotFoundError:
+    print(
+        "You need to install stashapp-tools. (https://pypi.org/project/stashapp-tools/)",
+        file=sys.stderr,
+    )
+    print(
+        "If you have pip (normally installed with python), run this command in a terminal (cmd): 'pip install stashapp-tools'",
+        file=sys.stderr,
+    )
+    sys.exit()
 
 # Constants
 PER_PAGE = 25
-DEFAULT_GRAPHQL_URL = "http://localhost:9999/graphql"
+STASHDB_ENDPOINT = "https://stashdb.org/graphql"
 
 
 def gql_query(endpoint, query, variables=None, api_key=None):
@@ -26,36 +35,15 @@ def gql_query(endpoint, query, variables=None, api_key=None):
         return None
 
 
-def get_stash_connection_info():
-    """
-    Retrieves Stash connection details.
-    """
-    try:
-        # Parse connection details from stdin
-        json_input = json.loads(sys.stdin.read())
-        FRAGMENT_SERVER = json_input.get("server_connection")
-
-        stash = StashInterface(FRAGMENT_SERVER)
-        stash_config = stash.get_configuration()
-
-        api_key = stash_config.get("general", {}).get("apiKey")
-        if not api_key:
-            log.warning("Local API key not found. Proceeding without it.")
-
-        return FRAGMENT_SERVER, api_key
-    except Exception as e:
-        log.error(f"Error retrieving Stash connection info: {e}")
-        return None, None
-
-
 def query_local_performers():
     performers = stash.find_performers()
     performer_ids = []
 
     for performer in performers:
         for stash_id in performer["stash_ids"]:
-            if stash_id["endpoint"] == "https://stashdb.org/graphql":
+            if stash_id["endpoint"] == STASHDB_ENDPOINT:
                 performer_ids.append(stash_id["stash_id"])
+    log.debug(f"Found {len(performer_ids)} performers in Stash.")
     return performer_ids
 
 
@@ -76,6 +64,45 @@ def query_stashdb_performers():
                 performers {
                     id
                     name
+                    disambiguation
+                    aliases
+                    gender
+                    urls {
+                        url
+                        type
+                    }
+                    birthdate {
+                        date
+                    }
+                    death_date
+                    ethnicity
+                    country
+                    eye_color
+                    hair_color
+                    height
+                    measurements {
+                        cup_size
+                        band_size
+                        waist
+                        hip
+                    }
+                    breast_type
+                    career_start_year
+                    career_end_year
+                    tattoos {
+                        location
+                        description
+                    }
+                    piercings {
+                        location
+                        description
+                    }
+                    images {
+                        id
+                        url
+                        width
+                        height
+                    }
                 }
             }
         }
@@ -89,7 +116,7 @@ def query_stashdb_performers():
             STASHDB_ENDPOINT,
             query,
             {"page": page, "per_page": PER_PAGE},
-            STASHDB_API_KEY,
+            stashdb_api_key,
         )
         if result:
             performer_data = result["data"]["queryPerformers"]
@@ -103,7 +130,26 @@ def query_stashdb_performers():
             page += 1
         else:
             break
+    log.debug(f"Found {len(performers)} favorite performers in StashDB.")
     return performers
+
+
+def clean_aliases(name, aliases):
+    out_list = []
+    for a in aliases:
+        if a.lower() != name.lower():
+            out_list.append(a)
+    return out_list
+
+
+def calc_measurements(measurements):
+    if measurements is None:
+        return ""
+    if measurements["band_size"] and measurements["cup_size"] and measurements["waist"] and measurements["hip"]:
+        return f"{measurements["band_size"]}{measurements["cup_size"]}-{measurements["waist"]}-{measurements["hip"]}"
+    if measurements["band_size"] and measurements["cup_size"]:
+        return f"{measurements["band_size"]}{measurements["cup_size"]}"
+    return ""
 
 
 def compare_performers(local_performers, stashdb_performers):
@@ -112,14 +158,87 @@ def compare_performers(local_performers, stashdb_performers):
         for performer in stashdb_performers
         if performer["id"] not in local_performers
     ]
-    log.info(f"Found {len(missing_performers)} missing performers.")
+    log.debug(f"Found {len(missing_performers)} missing performers.")
     return missing_performers
 
 
-def importFavorites():
-    local_performers = query_local_performers()
+def convert_stashdb_performance_to_stash(performer):
+    stash_performer = {
+        "name": performer["name"],
+        "alias_list": clean_aliases(performer["name"], performer["aliases"]),
+        "birthdate": performer["birthdate"]["date"] if performer["birthdate"] else None,
+        "career_length": format_career(
+            performer["career_start_year"], performer["career_end_year"]
+        ),
+        "country": performer["country"],
+        "death_date": performer["death_date"],
+        "disambiguation": performer["disambiguation"],
+        "ethnicity": convert_title_case(performer["ethnicity"]),
+        "eye_color": convert_title_case(performer["eye_color"]),
+        "fake_tits": convert_title_case(performer["breast_type"]),
+        "gender": performer["gender"],
+        "hair_color": convert_title_case(performer["hair_color"]),
+        "height_cm": performer["height"],
+        "image": performer["images"][0]["url"],
+        "measurements": calc_measurements(performer["measurements"]),
+        "piercings": convert_dictlist_to_string(
+            performer["piercings"], "location", "description"
+        ),
+        "tattoos": convert_dictlist_to_string(
+            performer["tattoos"], "location", "description"
+        ),
+        "urls": convert_dictlist_to_list(performer["urls"], "url"),
+        "stash_ids": [{"endpoint": STASHDB_ENDPOINT, "stash_id": performer["id"]}],
+    }
+    return stash_performer
 
-    log.info(f"Found {len(local_performers)} performers in Stash.")
+
+def convert_dictlist_to_list(dict, field):
+    if dict is None:
+        return None
+    out_list = []
+    for item in dict:
+        out_list.append(item[field])
+    return out_list
+
+
+def convert_dictlist_to_string(dict, field1, field2):
+    if dict is None:
+        return None
+    out_list = []
+    for item in dict:
+        pair = []
+        if item[field1]:
+            pair.append(item[field1])
+        if item[field2]:
+            pair.append(item[field2])
+        out_list.append(", ".join(pair))
+    return "; ".join(out_list)
+
+
+def convert_title_case(string):
+    return string.title() if string is not None else None
+
+
+def format_career(start, end):
+    if start is None:
+        return None
+    if end is None:
+        return f"{start} -"
+    return f"{start} - {end}"
+
+
+def get_stashbox_config(endpoint):
+    stash_boxes = stash.get_configuration().get("general", {}).get("stashBoxes")
+    stash_box_config = [box for box in stash_boxes if box["endpoint"] == endpoint]
+    return stash_box_config[0]
+
+
+def run_import():
+    global stashdb_api_key
+    stashdb_api_key = get_stashbox_config(STASHDB_ENDPOINT)["api_key"]
+
+    local_performers = query_local_performers()
 
     stashdb_performers = query_stashdb_performers()
     log.info(f"Found {len(stashdb_performers)} favorite performers in StashDB.")
@@ -127,52 +246,22 @@ def importFavorites():
     missing_performers = compare_performers(local_performers, stashdb_performers)
 
     if stashdb_performers:
-        for performer in missing_performers:
-            create_performer = {
-                "name": performer["name"],
-                "stash_ids": {
-                    "stash_id": performer["id"],
-                    "endpoint": STASHDB_ENDPOINT,
-                },
-            }
+        for index, performer in enumerate(missing_performers, start=1):
+            create_performer = convert_stashdb_performance_to_stash(performer)
             result = stash.create_performer(create_performer)
-
-            scrapedPerformer = stash.scrape_performer(
-                {"stash_box_endpoint": STASHDB_ENDPOINT}, result["id"]
-            )[0]
-
-            scrapedPerformer["id"] = result["id"]
-            scrapedPerformer["alias_list"] = scrapedPerformer["aliases"]
-            scrapedPerformer["height_cm"] = scrapedPerformer["height"]
-            scrapedPerformer["image"] = scrapedPerformer["images"][0]
-            scrapedPerformer["tag_ids"] = scrapedPerformer["tags"]
-
-            del scrapedPerformer["aliases"]
-            del scrapedPerformer["height"]
-            del scrapedPerformer["images"]
-            del scrapedPerformer["remote_site_id"]
-            del scrapedPerformer["stored_id"]
-            del scrapedPerformer["tags"]
-            log.debug(f"Scraped performer: {scrapedPerformer}")
-
-            stash.update_performer(scrapedPerformer)
-            # log.info(f"Created performer: {result}")
+            if result:
+                if "id" in result and "name" in result:
+                    log.info(
+                        f"Created performer: {result['name']} with ID: {result['id']}"
+                    )
+            else:
+                log.error(f"Failed to create performer: {performer['name']}")
+            log.progress(index / len(missing_performers))
 
 
 json_input = json.loads(sys.stdin.read())
 FRAGMENT_SERVER = json_input["server_connection"]
 stash = StashInterface(FRAGMENT_SERVER)
 
-LOCAL_API_KEY = stash.get_configuration().get("general", {}).get("apiKey")
-log.debug(f"Local API Key: {LOCAL_API_KEY}")
-STASHDB_API_KEY = (
-    stash.get_configuration()
-    .get("plugins", {})
-    .get("importFavoritePerformers", {})
-    .get("StashDb Api Key")
-)
-STASHDB_ENDPOINT = "https://stashdb.org/graphql"
-log.debug(f"StashDB API Key: {STASHDB_API_KEY}")
-
 if __name__ == "__main__":
-    importFavorites()
+    run_import()
